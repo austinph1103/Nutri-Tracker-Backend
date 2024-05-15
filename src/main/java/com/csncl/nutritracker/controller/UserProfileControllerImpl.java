3e63a4e047aa548ce103fb7ffee494f242b2ac01
An Pham <austinph@my.yorku.ca>
2024-05-15 11:12:36 +0700

    setup for endpoints creation (code cleaned up)

--- src/main/java/com/csncl/nutritracker/controller/UserProfileControllerImpl.java
+++ src/main/java/com/csncl/nutritracker/controller/UserProfileControllerImpl.java
@@ -1,18 +1,15 @@
-package com.csncl.nutritracker;
+package com.csncl.nutritracker.controller;
 
+import com.csncl.nutritracker.UserProfileApiDelegate;
+import com.csncl.nutritracker.model.UserProfile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
-@RestController
-@RequestMapping("")
-public class  ApiControllerImpl implements UserprofileApiDelegate {
+public class UserProfileControllerImpl implements UserProfileApiDelegate {
 
     @Override
     public ResponseEntity<Void> createUser() {
@@ -25,3 +22,4 @@ public class  ApiControllerImpl implements UserprofileApiDelegate {
         return ResponseEntity.of(Optional.of(new UserProfile(2, "ANBOB")));
     }
 }
+
